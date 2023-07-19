ba1d234f97b4279a988baa4d50d3223bde2053f0
jack-jm <101677953+jack-jm@users.noreply.github.com>
2023-07-19 03:07:20 +0000

    Just need to test component 4 - thought a bit more about how I could make my program better also.

--- 04_GetIngredientDetails_v1.py
+++ 04_GetIngredientDetails_v1.py
@@ -1,3 +1,37 @@
 # Functions go here
+def not_blank(question, error):
+  valid = False
+  while not valid:
+    response = input(question)
+
+    if response == "":
+      print("{}. \nPlease try again.\n".format(error))
+      continue
+
+    return response
+
+def num_check(question, error, num_type):
+  valid = False
+  while not valid:
+    try:
+      response = num_type(input(question))
+      if response <= 0:
+        print(error)
+      else:
+        return response
+    
+    except ValueError:
+      print(error)
 
 # Main routine goes here
+print("Please enter the ingredients in your recipe below. Type 'xxx' to finish entering ingredients.")
+
+ingredient_name = not_blank("Ingredient Name:", "Please enter the ingredient name")
+
+quantity = num_check("Quantity (Number of units):", "Please enter a valid number", float)
+
+unit_price = num_check("Price per unit:", "Please etner a valid number", int)
+
+price = quantity * unit_price
+
+print(price)
